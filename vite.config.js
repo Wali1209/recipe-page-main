59b307ec0bb4f8d05c559093bfad39cd732bf870
wali1209 <walibilal1209@gmail.com>
2024-02-15 19:58:05 +0500

    dist updated!

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/recipe-page-main',
+  base: '/recipe-page-main/',
   plugins: [react()],
 })
